0ce765fc3c8fceb4958ec7f117c3b69b377e89a7
Chad Steele <zippyskippy@gmail.com>
2020-04-17 15:41:53 -0600

    restored, sticking with search/tweets

--- src/lambda/tweets.js
+++ src/lambda/tweets.js
@@ -1,28 +1,22 @@
-
 var Twitter = require('twitter');
 var config = require('./config.js');
 var T = new Twitter(config);
-var lastStamp = 0;
 
 exports.handler = function (event, context, callback) {
+
     const q = event.queryStringParameters.q || '*';
     const count = event.queryStringParameters.count || 20;
     const type = event.queryStringParameters.type || 'recent';
-    const screen_name = event.queryStringParameters.screen_name || null;
 
     // Set up your search parameters
     var params = {
         q: q,
         count: count,
         result_type: type,
-        lang: 'en'
     }
-    if (screen_name) params.screen_name = screen_name;
-
-    const path = (screen_name) ? "statuses/user_timeline" : "search/tweets";
 
     // Initiate your search using the above paramaters
-    T.get(path, params, function (err, data, response) {
+    T.get('search/tweets', params, function (err, data, response) {
         // If there is no error, proceed
         if (!err) {
             callback(null, {
