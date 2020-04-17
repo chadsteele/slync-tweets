0d824791ff92876f3115988981c61fc0d8b79719
Chad Steele <zippyskippy@gmail.com>
2020-04-17 14:55:14 -0600

    bug fix

--- src/lambda/tweets.js
+++ src/lambda/tweets.js
@@ -8,7 +8,7 @@ exports.handler = function (event, context, callback) {
     const q = event.queryStringParameters.q || '*';
     const count = event.queryStringParameters.count || 20;
     const type = event.queryStringParameters.type || 'recent';
-    const screen_name = event.queryStringParameters.screen_name || null);
+    const screen_name = event.queryStringParameters.screen_name || null;
 
     // Set up your search parameters
     var params = {
