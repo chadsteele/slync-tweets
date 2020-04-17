a2076c265e17cffdfaf6fe584a5ad2eb4ce4b8d9
Chad Steele <zippyskippy@gmail.com>
2020-04-16 18:31:30 -0600

    init

---
+++ src/lambda/config.js
@@ -0,0 +1,15 @@
+// config.js
+module.exports = {
+    consumer_key: 'Z7TTtqa0CUSWMyZUH5f0FwzuN',
+    consumer_secret: 'zKKz3bSgt6sql73WclUm9kUsBzAv8rTgFBGmHhlNF056FkQ6Ek',
+    access_token_key: '22861563-C2iO49SGx861BDpRfb6DrAP4jM1WaqK951kRNAUAp',
+    access_token_secret: '2gWFJKk22nkE4XDV0apY9LPTlhwZI6ngKUHKiE3maPHWQ'
+  }
+
+  /*
+    
+API key: Z7TTtqa0CUSWMyZUH5f0FwzuN
+API secret key: zKKz3bSgt6sql73WclUm9kUsBzAv8rTgFBGmHhlNF056FkQ6Ek
+Access token: 22861563-C2iO49SGx861BDpRfb6DrAP4jM1WaqK951kRNAUAp
+Access token secret: 2gWFJKk22nkE4XDV0apY9LPTlhwZI6ngKUHKiE3maPHWQ
+  */
---
+++ src/lambda/tweets.js
@@ -0,0 +1,35 @@
+
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
+
+exports.handler = function (event, context, callback) {
+
+    const q = event.queryStringParameters.q || '*';
+    const count = event.queryStringParameters.count || 20;
+    const type = event.queryStringParameters.type || 'recent';
+
+    // Set up your search parameters
+    var params = {
+        q: q,
+        count: count,
+        result_type: type,
+        lang: 'en'
+    }
+
+    // Initiate your search using the above paramaters
+    T.get('search/tweets', params, function (err, data, response) {
+        // If there is no error, proceed
+        if (!err) {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(data.statuses)
+            });
+        } else {
+            callback(null, {
+                statusCode: 400,
+                body: JSON.stringify(err)
+            });
+        }
+    });
+}
